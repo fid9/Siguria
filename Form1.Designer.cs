5a8181080d3b5f970df6056346aa9fe65b17f547
fid9 <33783157+fid9@users.noreply.github.com>
2018-05-14 23:46:12 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Chat_Aplikacioni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textLocalPort = new System.Windows.Forms.TextBox();
+            this.textLocalIp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textRemotePort = new System.Windows.Forms.TextBox();
+            this.textRemoteIp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.listMessage = new System.Windows.Forms.ListBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.txtEnkripto = new System.Windows.Forms.TextBox();
+            this.btnEnkripto = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEnkriptuar = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textLocalPort);
+            this.groupBox1.Controls.Add(this.textLocalIp);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(37, 27);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.groupBox1.Size = new System.Drawing.Size(233, 117);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Une";
+            // 
+            // textLocalPort
+            // 
+            this.textLocalPort.Location = new System.Drawing.Point(104, 71);
+            this.textLocalPort.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.textLocalPort.Name = "textLocalPort";
+            this.textLocalPort.Size = new System.Drawing.Size(115, 20);
+            this.textLocalPort.TabIndex = 3;
+            // 
+            // textLocalIp
+            // 
+            this.textLocalIp.Location = new System.Drawing.Point(104, 23);
+            this.textLocalIp.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.textLocalIp.Name = "textLocalIp";
+            this.textLocalIp.Size = new System.Drawing.Size(115, 20);
+            this.textLocalIp.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textRemotePort);
+            this.groupBox2.Controls.Add(this.textRemoteIp);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(341, 27);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.groupBox2.Size = new System.Drawing.Size(235, 117);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Shoku";
+            // 
+            // textRemotePort
+            // 
+            this.textRemotePort.Location = new System.Drawing.Point(94, 80);
+            this.textRemotePort.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.textRemotePort.Name = "textRemotePort";
+            this.textRemotePort.Size = new System.Drawing.Size(128, 20);
+            this.textRemotePort.TabIndex = 3;
+            // 
+            // textRemoteIp
+            // 
+            this.textRemoteIp.Location = new System.Drawing.Point(94, 20);
+            this.textRemoteIp.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.textRemoteIp.Name = "textRemoteIp";
+            this.textRemoteIp.Size = new System.Drawing.Size(128, 20);
+            this.textRemoteIp.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "IP";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(601, 55);
+            this.buttonConnect.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(135, 59);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Lidhu";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(37, 355);
+            this.textMessage.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(573, 20);
+            this.textMessage.TabIndex = 3;
+            // 
+            // listMessage
+            // 
+            this.listMessage.FormattingEnabled = true;
+            this.listMessage.Location = new System.Drawing.Point(37, 176);
+            this.listMessage.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.listMessage.Name = "listMessage";
+            this.listMessage.Size = new System.Drawing.Size(699, 160);
+            this.listMessage.TabIndex = 4;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(626, 355);
+            this.buttonSend.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(110, 23);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "Dergo";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            this.buttonSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonSend_KeyDown);
+            // 
+            // txtEnkripto
+            // 
+            this.txtEnkripto.Location = new System.Drawing.Point(37, 419);
+            this.txtEnkripto.Name = "txtEnkripto";
+            this.txtEnkripto.Size = new System.Drawing.Size(573, 20);
+            this.txtEnkripto.TabIndex = 6;
+            // 
+            // btnEnkripto
+            // 
+            this.btnEnkripto.Location = new System.Drawing.Point(626, 416);
+            this.btnEnkripto.Name = "btnEnkripto";
+            this.btnEnkripto.Size = new System.Drawing.Size(110, 23);
+            this.btnEnkripto.TabIndex = 7;
+            this.btnEnkripto.Text = "Enkripto";
+            this.btnEnkripto.UseVisualStyleBackColor = true;
+            this.btnEnkripto.Click += new System.EventHandler(this.btnEnkripto_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 442);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Teksti i enkriptuar";
+            // 
+            // txtEnkriptuar
+            // 
+            this.txtEnkriptuar.Location = new System.Drawing.Point(37, 458);
+            this.txtEnkriptuar.Name = "txtEnkriptuar";
+            this.txtEnkriptuar.Size = new System.Drawing.Size(573, 20);
+            this.txtEnkriptuar.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 400);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Teksti per enkriptim";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 492);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEnkriptuar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnEnkripto);
+            this.Controls.Add(this.txtEnkripto);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.listMessage);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Name = "Form1";
+            this.Text = "ChatApps";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textLocalPort;
+        private System.Windows.Forms.TextBox textLocalIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textRemotePort;
+        private System.Windows.Forms.TextBox textRemoteIp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.ListBox listMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox txtEnkripto;
+        private System.Windows.Forms.Button btnEnkripto;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEnkriptuar;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.IO;
+
+
+
+namespace Chat_Aplikacioni
+{
+    public partial class Form1 : Form
+    {
+        Socket sck;
+        EndPoint epLocal, epRemote;
+        byte[] buffer;
+        public Form1()
+        {
+            InitializeComponent();
+            aesOb = Rijndael.Create();
+            
+        }
+
+        string cipherData;
+        byte[] cipherbytes;
+        byte[] plainbytes;
+        byte[] plainbytes2;
+        byte[] plainKey;
+
+        SymmetricAlgorithm aesOb;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            textLocalIp.Text = GetLocalIP();
+            textRemoteIp.Text = GetLocalIP();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            epLocal = new IPEndPoint(IPAddress.Parse(textLocalIp.Text), Convert.ToInt32(textLocalPort.Text));
+            sck.Bind(epLocal);
+
+            epRemote = new IPEndPoint(IPAddress.Parse(textRemoteIp.Text), Convert.ToInt32(textRemotePort.Text));
+            sck.Connect(epRemote);
+            buffer = new byte[1500];
+            sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+        }
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                byte[] receivedData = new byte[1500];
+                receivedData = (byte[])aResult.AsyncState;
+                ASCIIEncoding aEncoding = new ASCIIEncoding();
+                string receivedMessage = aEncoding.GetString(receivedData);
+                listMessage.Items.Add("Shoku: " + receivedMessage);
+                buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            ASCIIEncoding aEncoding = new ASCIIEncoding();
+            byte[] sendingMessage = new byte[1500];
+            sendingMessage = aEncoding.GetBytes(textMessage.Text);
+            sck.Send(sendingMessage);
+            listMessage.Items.Add("Une: " + textMessage.Text);
+            textMessage.Text = "";
+        }
+
+        private void buttonSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonSend.PerformClick();
+            }
+        }
+
+        private void btnEnkripto_Click(object sender, EventArgs e)
+        {
+            cipherData = txtEnkripto.Text;
+            plainbytes = Encoding.UTF8.GetBytes(cipherData);
+            plainKey = Encoding.UTF8.GetBytes("1234567891234567");
+            aesOb.Key = plainKey;
+            aesOb.Mode = CipherMode.CBC;
+            aesOb.Padding = PaddingMode.PKCS7;
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, aesOb.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(plainbytes, 0, plainbytes.Length);
+            cs.Close();
+            cipherbytes = ms.ToArray();
+            ms.Close();
+            txtEnkriptuar.Text = Encoding.UTF8.GetString(cipherbytes);
+        }
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily ==AddressFamily.InterNetwork)
+                    return ip.ToString();
+            }
+            return "127.0.0.1";
+
+
+        }
+    }
+}
